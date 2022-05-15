548134ea42aab5943d7721be98f1ba9902ed5cd1
AACoderBeg <abrarahmad.cs@gmail.com>
2022-05-15 10:49:41 -0500

    Added more files- Okta Login/Logout Security

---
+++ 03-FrontEnd/ecommerceAngular/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa51a9p4tBJ3eVEQ5d7',
+        issuer: 'https://dev-29379947.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}

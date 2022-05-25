ef1633c545a54632fff5bc937aa5bd7f2eb71495
AACoderBeg <abrarahmad.cs@gmail.com>
2022-05-25 16:56:15 -0500

    Added files for Backend support of Stripe

--- 02-BackEnd/eCommerceSpring/src/main/java/com/hcl/config/SecurityConfig.java
+++ 02-BackEnd/eCommerceSpring/src/main/java/com/hcl/config/SecurityConfig.java
@@ -1,4 +1,30 @@
 package com.hcl.config;
 
-public class SecurityConfig {
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Protecting the endpoint /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        //Adding CORS filters
+        http.cors();
+
+        //force a non-empty response body for 401's to make a more friendly response
+        Okta.configureResourceServer401ResponseBody(http);
+
+        //Disabling CSRF since we're not using cookies
+        http.csrf().disable();
+    }
 }
--- 02-BackEnd/eCommerceSpring/src/main/java/com/hcl/dto/PaymentInfo.java
+++ 02-BackEnd/eCommerceSpring/src/main/java/com/hcl/dto/PaymentInfo.java
@@ -1,2 +1,9 @@
-package com.hcl.dto;public class PaymentInfo {
+package com.hcl.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentInfo {
+    private int amount;
+    private String currency;
 }
